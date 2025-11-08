8c06756c948a6c405d26584826f618b5f73f7ea4
Dhruv Shah <dhruvkshah75@gmail.com>
2025-11-08 12:32:02 +0530

    made changes to .gitignore and some more changes to the main.py

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
+
+# list to store data of each posts
+# hardcoded 2 values 
+my_posts = [
+    {
+        "title": "title of post 1",
+        "content": "content of post 1",
+        "id": 1
+    },
+    {
+        "title": "favourite foods",
+        "content": "I like pizza",
+        "id": 2
+    }
+]  
 
 # this is called path operation 
 @app.get("/")
@@ -16,7 +32,7 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {
-        "data": "This is your post"
+        "data": my_posts
     }
 
 # class post extends the BaseModel
@@ -26,10 +42,21 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None   # a completely optional feild 
 
-@app.post("/create_posts")
-def create_posts(data: Post):
-    print(data)   
-    print(data.title)
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 100000)
+    # post.model_dump()  # post.dict() the dict has been depricated 
+    my_posts.append(post_dict)  
     return {
-        "new_post": f"title: {data.title}, content: {data.content}"
+        "data" : post_dict
+    }
+
+
+
+@app.get("/posts/{id}")
+def get_posts(id):
+    print(id)
+    return {
+        "post detail": f"Here is the post with the {id}"
     }
