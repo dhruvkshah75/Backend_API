94ca26e8710634a8f71ced8b93335a5af4579e5c
Dhruv Shah <dhruvkshah75@gmail.com>
2025-11-17 21:34:51 +0530

    Added the check that account with the email already exists

--- app/routers/user.py
+++ app/routers/user.py
@@ -11,11 +11,19 @@ router = APIRouter(
 # USER crud operations
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
+    """
+    Check if the user already exists my checking the database or else throw a message with Email already registered
+    """
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Email already registered")
 
     # hash the password 
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.model_dump())
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
